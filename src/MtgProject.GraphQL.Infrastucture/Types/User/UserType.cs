0cced5cc40894476cf95bba56a7cdeb38b201c27
Ondřej Slimák <ondrej.slimak@riganti.cz>
2020-08-30 23:57:58 +0200

    fix userType

--- src/MtgProject.GraphQL.Infrastucture/Types/User/UserType.cs
+++ src/MtgProject.GraphQL.Infrastucture/Types/User/UserType.cs
@@ -12,18 +12,13 @@ namespace MtgProject.GraphQL.Infrastructure.Types.User
              
              descriptor
                  .Field(t => t.Decks)
-                 //.Resolver(ctx => ctx.Service<MtgDbContext>().Decks.Where(d => ctx.Parent<UserEntity>().Id == d.UserId))
-                 .ResolveWith<DeckResolvers>(d => d.GetDecksByUserAsync(default!, default!, default))
                  .Name("decks")
                  .UseFiltering()
                  .UseSorting()
                  .UseSelection();
 
-             //TODO: add resolver with data loader
              descriptor
                  .Field(t => t.GameParticipations)
-                 //.Resolver(ctx => ctx.Service<MtgDbContext>().GameParticipations.Where(d => ctx.Parent<UserEntity>().Id == d.UserId))
-                 .ResolveWith<GameParticipationResolvers>(d => d.GetGameParticiptionsByUserAsync(default!, default!, default))
                  .Name("gameParticipations")
                  .UseFiltering()
                  .UseSorting()
